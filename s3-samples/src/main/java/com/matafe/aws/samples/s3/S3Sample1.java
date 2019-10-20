5963b46411e50d021ecc12558731ae9731926738
Mauricio Ferraz <matafe@gmail.com>
2019-10-20 15:42:04 +0100

    initial s3 commit - first sample

---
+++ s3-samples/src/main/java/com/matafe/aws/samples/s3/S3Sample1.java
@@ -0,0 +1,43 @@
+package com.matafe.aws.samples.s3;
+
+import java.io.InputStream;
+
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+
+public class S3Sample1 {
+
+	private static final String BUCKET_NAME = "matafe-bucket-sample";
+
+	private static AmazonS3 s3Client;
+
+	public static void main(String[] args) throws Exception {
+
+		s3Client = AmazonS3ClientBuilder.standard().withCredentials(new ProfileCredentialsProvider()).build();
+
+		// create bucket if does not exists
+		if (!s3Client.doesBucketExistV2(BUCKET_NAME)) {
+			s3Client.createBucket(BUCKET_NAME);
+			System.out.println("Bucket created: " + BUCKET_NAME);
+		}
+
+		final String fileName = "sample1.txt";
+
+		InputStream is = null;
+		try {
+			is = S3Sample1.class.getResourceAsStream("/sample1.txt");
+			System.out.println(String.format("Put file: %s", new Object[] { args }));
+			ObjectMetadata md = new ObjectMetadata();
+			md.setContentType("text/plain");
+			s3Client.putObject(new PutObjectRequest(BUCKET_NAME, fileName, is, md ));
+		} finally {
+			if (is != null) {
+				is.close();
+			}
+		}
+	}
+
+}
